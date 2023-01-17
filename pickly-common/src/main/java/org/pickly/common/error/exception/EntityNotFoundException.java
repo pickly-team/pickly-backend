025893dcb4dea897281e806af7dc6f860c28e8e6
Ueibin <ats1113@zuminternet.com>
2023-01-18 01:10:03 +0900

    feat : Error handler in common module

---
+++ pickly-common/src/main/java/org/pickly/common/error/exception/EntityNotFoundException.java
@@ -0,0 +1,8 @@
+package org.pickly.common.error.exception;
+
+public class EntityNotFoundException extends BusinessException {
+
+  public EntityNotFoundException(String message) {
+    super(message, ErrorCode.ENTITY_NOT_FOUND);
+  }
+}
