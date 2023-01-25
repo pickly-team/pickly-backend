18a26cf853c0c612d7bf10a60c4c1a4bff3c9d27
Ueibin Kim <at365@naver.com>
2023-01-26 00:06:24 +0900

    Revert "feat : Error handler in common module"
    This reverts commit 025893dcb4dea897281e806af7dc6f860c28e8e6.

--- pickly-common/src/main/java/org/pickly/common/error/exception/EntityNotFoundException.java
+++
@@ -1,8 +0,0 @@
-package org.pickly.common.error.exception;
-
-public class EntityNotFoundException extends BusinessException {
-
-  public EntityNotFoundException(String message) {
-    super(message, ErrorCode.ENTITY_NOT_FOUND);
-  }
-}
