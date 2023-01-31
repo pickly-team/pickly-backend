311d1f47c4e87b09f81fea658a03681da072be23
EunjiShin <eunji980310@gmail.com>
2023-01-31 12:11:32 +0900

    chore : import 패키지 경로 수정

--- pickly-service/src/main/java/org/pickly/service/common/filter/CorsWebFilter.java
+++ pickly-service/src/main/java/org/pickly/service/common/filter/CorsWebFilter.java
@@ -4,12 +4,13 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.cors.CorsConfiguration;
 import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+import org.springframework.web.filter.CorsFilter;
 
 @Configuration
 public class CorsWebFilter {
 
 	@Bean
-	public org.springframework.web.filter.CorsFilter corsFilter() {
+	public CorsFilter corsFilter() {
 		CorsConfiguration config = new CorsConfiguration();
 		config.setAllowCredentials(true);
 		config.addAllowedOriginPattern("*");
