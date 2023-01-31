055026b52bd5243693ff6447da8c20d0e2319353
EunjiShin <eunji980310@gmail.com>
2023-01-31 12:23:51 +0900

    style : google checkstyle format 적용

--- pickly-service/src/main/java/org/pickly/service/common/config/SwaggerConfig.java
+++ pickly-service/src/main/java/org/pickly/service/common/config/SwaggerConfig.java
@@ -7,16 +7,16 @@ import io.swagger.v3.oas.annotations.info.Info;
 import lombok.RequiredArgsConstructor;
 
 @OpenAPIDefinition(
-	info = @Info(
-		title = "Pickly 서비스 API 명세서",
-		description = "즐겨찾기 관리 서비스 Pickly API 명세서",
-		version = "v1"
-	)
+    info = @Info(
+        title = "Pickly 서비스 API 명세서",
+        description = "즐겨찾기 관리 서비스 Pickly API 명세서",
+        version = "v1"
+    )
 )
 @Configuration
 @RequiredArgsConstructor
 public class SwaggerConfig {
 
-	// 추후 인증 & 권한 전역 관리 설정 추가 예정
+  // 추후 인증 & 권한 전역 관리 설정 추가 예정
 
 }
--- pickly-service/src/main/java/org/pickly/service/test/dto/TestDto.java
+++ pickly-service/src/main/java/org/pickly/service/test/dto/TestDto.java
@@ -17,14 +17,14 @@ import lombok.NoArgsConstructor;
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
 public class TestDto {
 
-	@NotBlank(message = "값을 넣어주세요~")
-	@Length(min = 1, max = 50, message = "50자 제한이에요~")
-	@Schema(description = "이름", example = "스웨거", required = true)
-	private String name;
+  @NotBlank(message = "값을 넣어주세요~")
+  @Length(min = 1, max = 50, message = "50자 제한이에요~")
+  @Schema(description = "이름", example = "스웨거", required = true)
+  private String name;
 
-	@NotBlank(message = "패턴 예시~")
-	@Pattern(regexp = "[A-Z\\d]{3}", message = "대문자 숫자 조합 3글자~")
-	@Schema(description = "패턴", example = "AB1", required = true)
-	private String pattern;
+  @NotBlank(message = "패턴 예시~")
+  @Pattern(regexp = "[A-Z\\d]{3}", message = "대문자 숫자 조합 3글자~")
+  @Schema(description = "패턴", example = "AB1", required = true)
+  private String pattern;
 
 }
