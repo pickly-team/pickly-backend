04f866ed50f53a0d5a64a0946971b8ace4e1c196
EunjiShin <eunji980310@gmail.com>
2023-01-31 12:13:07 +0900

    fix : RequestBody 항목에 @Valid 추가

--- pickly-service/src/main/java/org/pickly/service/test/controller/TestCtrl.java
+++ pickly-service/src/main/java/org/pickly/service/test/controller/TestCtrl.java
@@ -13,6 +13,7 @@ import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.Parameter;
 import io.swagger.v3.oas.annotations.responses.ApiResponse;
 import io.swagger.v3.oas.annotations.responses.ApiResponses;
+import jakarta.validation.Valid;
 import jakarta.validation.constraints.Positive;
 import lombok.RequiredArgsConstructor;
 
@@ -31,7 +32,7 @@ public class TestCtrl {
 	public ResponseEntity<Void> test(
 		@Parameter(name = "testId", description = "테스트에용", example = "1", required = true)
 		@Positive(message = "테스트 ID는 0보다 커야 합니다.") @PathVariable final long testId,
-		@Parameter @RequestBody TestDto testDto
+		@Parameter @RequestBody @Valid TestDto testDto
 	) {
 		return ResponseEntity.ok().build();
 	}
