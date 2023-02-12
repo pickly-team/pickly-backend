8d2dde731becceb1027efda0c9d181d5c35285b9
EunjiShin <eunji980310@gmail.com>
2023-02-12 15:37:30 +0900

    feat : BaseEntity 생성

--- pickly-service/src/main/java/org/pickly/service/ServiceApplication.java
+++ pickly-service/src/main/java/org/pickly/service/ServiceApplication.java
@@ -2,8 +2,10 @@ package org.pickly.service;
 
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.context.properties.ConfigurationPropertiesScan;
 
 @SpringBootApplication
+@ConfigurationPropertiesScan
 public class ServiceApplication {
 
   private static final String DEV_PROPERTIES = "optional:pickly-service/src/main/resources/application-dev.yml";
---
+++ pickly-service/src/main/java/org/pickly/service/common/config/JpaAuditingConfig.java
@@ -0,0 +1,11 @@
+package org.pickly.service.common.config;
+
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
+
+@Configuration
+@EnableJpaAuditing
+public class JpaAuditingConfig {
+
+}
