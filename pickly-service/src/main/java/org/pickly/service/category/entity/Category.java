984206240ebad7f525d56dd04fcfe00173814d4b
EunjiShin <eunji980310@gmail.com>
2023-02-12 16:02:20 +0900

    feat : Category Entity 추가

--- pickly-service/src/main/java/org/pickly/service/category/entity/Category.java
+++ pickly-service/src/main/java/org/pickly/service/category/entity/Category.java
@@ -1,6 +1,10 @@
 package org.pickly.service.category.entity;
 
+import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
+import jakarta.persistence.FetchType;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
 import jakarta.persistence.Table;
 import lombok.AccessLevel;
 import lombok.AllArgsConstructor;
@@ -8,6 +12,7 @@ import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import org.pickly.service.common.utils.base.BaseEntity;
+import org.pickly.service.member.entity.Member;
 
 @Entity
 @Getter
@@ -16,5 +21,12 @@ import org.pickly.service.common.utils.base.BaseEntity;
 @Table(name = "member")
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
 public class Category extends BaseEntity {
+
+  @ManyToOne(fetch = FetchType.LAZY)
+  @JoinColumn(name = "member_id")
+  private Member member;
+
+  @Column(length = 100, nullable = false, unique = true)
+  private String name;
 
 }
