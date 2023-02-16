3c7790ca0a994cf32f969d744d997cfc5ce7e33b
JerryK026 <kskyung0624@gmail.com>
2023-02-17 01:21:09 +0900

    test : 테스트에 필요한 CategoryFactory, MemberFactory를 생성한다

---
+++ pickly-service/src/test/groovy/org/pickly/service/member/MemberFactory.java
@@ -0,0 +1,17 @@
+package org.pickly.service.member;
+
+import org.pickly.service.member.entity.Member;
+import org.pickly.service.member.entity.Password;
+
+public class MemberFactory {
+  public Member testMember() {
+    return Member.builder()
+        .username("picko123")
+        .password(new Password("nobodyKnows123"))
+        .isHardMode(false)
+        .email("picko123@gmail.com")
+        .name("picko")
+        .nickname("iAmNotAPickyEater")
+        .build();
+  }
+}
