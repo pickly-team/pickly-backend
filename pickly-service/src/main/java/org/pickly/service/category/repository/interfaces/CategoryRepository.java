a4735d17a5498ef0954598e1f64fba8c539fa514
EunjiShin <eunji980310@gmail.com>
2023-02-22 22:27:55 +0900

    feat : queryDSL 설정 루트 모듈로 이동

--- pickly-service/src/main/java/org/pickly/service/category/repository/interfaces/CategoryRepository.java
+++ pickly-service/src/main/java/org/pickly/service/category/repository/interfaces/CategoryRepository.java
@@ -4,7 +4,4 @@ import org.pickly.service.category.entity.Category;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface CategoryRepository extends JpaRepository<Category, Long> {
-
-  Long save(Category category);
-
 }
