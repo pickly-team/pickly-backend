9dbeeafa12315c40c6486d0b5e0b88436c7c10f1
EunjiShin <eunji980310@gmail.com>
2023-02-22 23:14:17 +0900

    feat : GraphQL 예제 코드 추가

--- pickly-service/src/main/java/org/pickly/service/member/service/interfaces/MemberService.java
+++ pickly-service/src/main/java/org/pickly/service/member/service/interfaces/MemberService.java
@@ -1,5 +1,10 @@
 package org.pickly.service.member.service.interfaces;
 
+import java.util.List;
+import org.pickly.service.member.entity.Member;
+
 public interface MemberService {
+
+  List<Member> getMembers();
 
 }
