3005e191a3ee0e92f13a810577d4e3f93472c8b0
ww8007 <wshmin1234@gmail.com>
2023-03-01 23:19:31 +0900

    refactor: 단일 북마크를 찾는 findById 유틸 메서드 추가

--- pickly-service/src/main/java/org/pickly/service/bookmark/service/interfaces/BookmarkService.java
+++ pickly-service/src/main/java/org/pickly/service/bookmark/service/interfaces/BookmarkService.java
@@ -1,10 +1,13 @@
 package org.pickly.service.bookmark.service.interfaces;
 
 
+import org.pickly.service.bookmark.entity.Bookmark;
+
 public interface BookmarkService {
+
+  Bookmark findById(Long Id);
 
   void likeBookmark(Long memberId);
 
   void cancelLikeBookmark(Long memberId);
-
 }
