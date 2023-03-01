787150aa27be5db8f0fdc3c3ed1f77c7111063ac
ww8007 <wshmin1234@gmail.com>
2023-03-01 23:16:03 +0900

    refactor: @RestController 사용으로 ResponseEntity 삭제 @PutMapping -> @PostMapping 으로 변경, feat: 양수임을 보장하기 위해 bookmarkId @Positive 검증 추가

--- pickly-service/src/main/java/org/pickly/service/bookmark/controller/BookmarkController.java
+++ pickly-service/src/main/java/org/pickly/service/bookmark/controller/BookmarkController.java
@@ -2,13 +2,15 @@ package org.pickly.service.bookmark.controller;
 
 import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.media.Schema;
+import jakarta.validation.constraints.Positive;
 import lombok.RequiredArgsConstructor;
 import org.pickly.service.bookmark.service.interfaces.BookmarkService;
-import org.springframework.http.ResponseEntity;
+import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -19,30 +21,30 @@ public class BookmarkController {
 
   private final BookmarkService bookmarkService;
 
-
-  @PutMapping("/{bookmarkId}/like")
-  @Operation(summary = "Like bookmark", description = "Like bookmark")
-
   // TODO : 멤버 아이디를 토큰에서 가져오도록 수정
   // 또는 북마크 작업하시는 분이 멤버 아이디를 추가해 주시면 추가할 것
-  public ResponseEntity<Void> likeBookmark(
+  @PostMapping("/{bookmarkId}/like")
+  @ResponseStatus(value = HttpStatus.OK)
+  @Operation(summary = "Like bookmark", description = "Like bookmark")
+  public void likeBookmark(
       @PathVariable
+      @Positive
       @Schema(description = "Bookmark id", example = "1")
       Long bookmarkId
   ) {
     bookmarkService.likeBookmark(bookmarkId);
-    return ResponseEntity.ok().build();
   }
 
 
   @DeleteMapping("/{bookmarkId}/like")
+  @ResponseStatus(value = HttpStatus.OK)
   @Operation(summary = "CancelLike bookmark", description = "CancelLike bookmark")
-  public ResponseEntity<Void> cancelLikeBookmark(
+  public void cancelLikeBookmark(
       @PathVariable
+      @Positive
       @Schema(description = "Bookmark id", example = "1")
       Long bookmarkId
   ) {
     bookmarkService.cancelLikeBookmark(bookmarkId);
-    return ResponseEntity.ok().build();
   }
 }
