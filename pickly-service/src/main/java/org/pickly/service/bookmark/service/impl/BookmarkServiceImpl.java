17fb1c84a40fed7c2a841c442fab8291d09ba3ed
ww8007 <wshmin1234@gmail.com>
2023-03-01 14:30:34 +0900

    fix: Transactional 처리 삭제 https://github.com/pickly-team/pickly-backend/pull/37#discussion_r1121148339 refactor: unlike -> cancelLike 메서드 네이밍 변경 https://github.com/pickly-team/pickly-backend/pull/37#discussion_r1121071964

--- pickly-service/src/main/java/org/pickly/service/bookmark/service/impl/BookmarkServiceImpl.java
+++ pickly-service/src/main/java/org/pickly/service/bookmark/service/impl/BookmarkServiceImpl.java
@@ -6,7 +6,6 @@ import org.pickly.service.bookmark.entity.Bookmark;
 import org.pickly.service.bookmark.repository.interfaces.BookmarkRepository;
 import org.pickly.service.bookmark.service.interfaces.BookmarkService;
 import org.springframework.stereotype.Service;
-import org.springframework.transaction.annotation.Transactional;
 
 
 @Service
@@ -17,7 +16,6 @@ public class BookmarkServiceImpl implements BookmarkService {
 
 
   @Override
-  @Transactional
   public void likeBookmark(Long bookmarkId) {
     Bookmark bookmark = bookmarkRepository.findOneById(bookmarkId)
         .orElseThrow(() -> new EntityNotFoundException("존재하지 않는 북마크입니다."));
@@ -26,10 +24,10 @@ public class BookmarkServiceImpl implements BookmarkService {
   }
 
   @Override
-  public void unlikeBookmark(Long bookmarkId) {
+  public void cancelLikeBookmark(Long bookmarkId) {
     Bookmark bookmark = bookmarkRepository.findOneById(bookmarkId)
         .orElseThrow(() -> new EntityNotFoundException("존재하지 않는 북마크입니다."));
 
-    bookmark.updateUserUnlike();
+    bookmark.updateUserCancelLike();
   }
 }
