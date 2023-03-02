423e06f05ebac2fff8cd5d06859c41d6c4ed925b
kimueibin <at365@naver.com>
2023-03-03 02:21:02 +0900

    feat : 카테고리 Controller 내 Version 어노테이션 테스트를 위한 테스트 API 추가 ( 삭제 예정 )

--- pickly-service/src/main/java/org/pickly/service/category/controller/CategoryController.java
+++ pickly-service/src/main/java/org/pickly/service/category/controller/CategoryController.java
@@ -1,15 +1,24 @@
 package org.pickly.service.category.controller;
 
 import lombok.RequiredArgsConstructor;
+import version.VersionResource;
 import org.pickly.service.category.service.interfaces.CategoryService;
+import org.pickly.service.common.version.ApiVersion;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequiredArgsConstructor
-@RequestMapping("/api/v1")
+@RequestMapping("/api")
 public class CategoryController {
 
   private final CategoryService categoryService;
+
+  @GetMapping("/test")
+  @VersionResource(from = ApiVersion.V1)
+  public String test() {
+    return "test";
+  }
 
 }
