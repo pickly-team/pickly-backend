e6e0c254466273fc32446117b42210a340293720
kimueibin <at365@naver.com>
2023-03-03 02:15:59 +0900

    feat: API Version 클래스 추가

---
+++ pickly-service/src/main/java/org/pickly/service/common/version/ApiVersion.java
@@ -0,0 +1,13 @@
+package org.pickly.service.common.version;
+
+public class ApiVersion {
+
+  public static final String V1 = "1.0";
+  public static final String V2 = "2.0";
+  public static final String V3 = "3.0";
+
+  private ApiVersion() {
+    throw new AssertionError();
+  }
+
+}
