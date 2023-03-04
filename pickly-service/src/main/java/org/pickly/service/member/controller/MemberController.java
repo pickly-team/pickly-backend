02b7a96089507e575a0e1cd4dc7c02103e9fcebe
litsynp <nocte55is@gmail.com>
2023-03-04 16:42:25 +0900

    refactor: use simplified return type for controller

--- pickly-service/src/main/java/org/pickly/service/member/controller/MemberController.java
+++ pickly-service/src/main/java/org/pickly/service/member/controller/MemberController.java
@@ -8,7 +8,6 @@ import org.pickly.service.member.common.MemberMapper;
 import org.pickly.service.member.controller.request.MemberProfileUpdateReq;
 import org.pickly.service.member.controller.response.MemberProfileRes;
 import org.pickly.service.member.service.interfaces.MemberService;
-import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PutMapping;
@@ -28,7 +27,7 @@ public class MemberController {
 
   @PutMapping("/me")
   @Operation(summary = "Update my profile")
-  public ResponseEntity<Void> updateMyProfile(
+  public void updateMyProfile(
       // TODO: Replace with member ID from JWT or that from any other authentication method
       @RequestParam(value = "member_id")
       @Schema(description = "Member ID (should be replaced later on)", example = "1")
@@ -38,19 +37,17 @@ public class MemberController {
       MemberProfileUpdateReq request
   ) {
     memberService.updateMyProfile(memberId, memberMapper.toDTO(request));
-    return ResponseEntity.ok().build();
   }
 
   @GetMapping("/{memberId}")
   @Operation(summary = "Get member profile")
-  public ResponseEntity<MemberProfileRes> getMemberProfile(
+  public MemberProfileRes getMemberProfile(
       @PathVariable
       @Schema(description = "Member ID", example = "1")
       Long memberId
   ) {
-    MemberProfileRes response = memberMapper.toResponse(
+    return memberMapper.toResponse(
         memberService.findProfileByMemberId(memberId)
     );
-    return ResponseEntity.ok(response);
   }
 }
