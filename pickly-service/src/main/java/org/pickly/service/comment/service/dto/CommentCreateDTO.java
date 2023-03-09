fb5af15c2c42ecdc8e378896ee9d6ade56fe440e
EunjiShin <eunji980310@gmail.com>
2023-03-09 18:06:39 +0900

    refactor : CommentCreateRequest mapper 추가

--- pickly-service/src/main/java/org/pickly/service/comment/service/dto/CommentCreateDTO.java
+++ pickly-service/src/main/java/org/pickly/service/comment/service/dto/CommentCreateDTO.java
@@ -1,5 +1,12 @@
 package org.pickly.service.comment.service.dto;
 
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+
+@Getter
+@AllArgsConstructor
 public class CommentCreateDTO {
+
+  private String content;
 
 }
