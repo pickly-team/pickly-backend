e79b89a7e7e574567b86ed7f5e733ca9fd47d00e
kimueibin <at365@naver.com>
2023-03-10 02:22:54 +0900

    refactor : 설명주석 추가 + 검증로직 private 메서드로 분리

--- pickly-common/src/main/java/version/VersionRange.java
+++ pickly-common/src/main/java/version/VersionRange.java
@@ -1,5 +1,13 @@
 package version;
 
+import java.util.HashSet;
+import java.util.Set;
+import org.springframework.util.StringUtils;
+
+/*
+ * 지정된 from 버전과 to 버전 사이의 버전 범위를 나타내는 버전 범위 클래스입니다.
+ * 클래스는 주어진 버전이 지정된 범위 내에 있는지 확인하는 데 사용됩니다.
+ */
 public class VersionRange {
 
   private final Version from;
@@ -9,6 +17,23 @@ public class VersionRange {
   public VersionRange(String from, String to) {
     this.from = new Version(from);
     this.to = new Version(to);
+  }
+
+  public static Set<VersionRange> versionRange(String from, String to) {
+    Set<VersionRange> ranges = new HashSet<>();
+
+    addVersionRange(ranges, from, to);
+
+    return ranges;
+  }
+
+  private static void addVersionRange(Set<VersionRange> ranges, String from, String to) {
+    if (StringUtils.hasText(from)) {
+      String toVersion = (StringUtils.hasText(to) ? to : Version.MAX_VERSION);
+      VersionRange versionRange = new VersionRange(from, toVersion);
+
+      ranges.add(versionRange);
+    }
   }
 
   public boolean includes(String other) {
