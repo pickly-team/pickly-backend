77bf2c76dcd49e534b9fbb9e822258f44e2e02d6
kimueibin <at365@naver.com>
2023-03-10 02:23:59 +0900

    refactor : 클래스 순서 변경과 설명로직 추가 private 메서드 분리

--- pickly-common/src/main/java/version/VersionResourceRequestCondition.java
+++ pickly-common/src/main/java/version/VersionResourceRequestCondition.java
@@ -2,8 +2,6 @@ package version;
 
 import jakarta.servlet.http.HttpServletRequest;
 import java.util.Collection;
-import java.util.Collections;
-import java.util.HashSet;
 import java.util.LinkedHashSet;
 import java.util.Map;
 import java.util.Objects;
@@ -23,34 +21,100 @@ public class VersionResourceRequestCondition extends
 
   private final String acceptedMediaType;
 
+  /*
+   * @VersionResource 어노테이션의 media, from, to 속성을  받아서 VersionRange 객체를 생성한다.
+   */
   public VersionResourceRequestCondition(String acceptedMediaType, String from, String to) {
-    this(acceptedMediaType, versionRange(from, to));
+    this(acceptedMediaType, VersionRange.versionRange(from, to));
   }
+
 
   public VersionResourceRequestCondition(String acceptedMediaType,
       Collection<VersionRange> versions) {
     this.acceptedMediaType = acceptedMediaType;
-    this.versions = Collections.unmodifiableSet(new HashSet<VersionRange>(versions));
+    this.versions = Set.copyOf(versions);
   }
 
-  private static Set<VersionRange> versionRange(String from, String to) {
-    Set<VersionRange> ranges = new HashSet<>();
-
-    if (StringUtils.hasText(from)) {
-      String toVersion = (StringUtils.hasText(to) ? to : Version.MAX_VERSION);
-      VersionRange versionRange = new VersionRange(from, toVersion);
-
-      ranges.add(versionRange);
-    }
-
-    return ranges;
-  }
-
+  /*
+   * combine 메소드는 두 개의 VersionResourceRequestCondition 객체를 합쳐서 새로운 VersionResourceRequestCondition 객체를 생성한다.
+   */
   @Override
   public VersionResourceRequestCondition combine(VersionResourceRequestCondition other) {
     log.info("Combining:\n{}\n{}", this, other);
     Set<VersionRange> newVersions = new LinkedHashSet<VersionRange>(this.versions);
     newVersions.addAll(other.versions);
+
+    return new VersionResourceRequestCondition(combineMediaTypes(other), newVersions);
+  }
+
+  /*
+   * getMatchingCondition 메소드는 요청에 맞는 VersionResourceRequestCondition 객체를 반환한다.
+   * 요청에 맞는 VersionResourceRequestCondition 객체를 찾는 과정에서 Accept 헤더와 version 파라미터를 확인한다.
+   * Accept 헤더와 version 파라미터가 모두 매칭되지 않는다면 null을 반환한다.
+   * Accept 헤더와 version 파라미터가 모두 매칭된다면 현재 VersionResourceRequestCondition 객체를 반환한다.
+   */
+  @Override
+  public VersionResourceRequestCondition getMatchingCondition(HttpServletRequest request) {
+
+    String accept = Optional.ofNullable(request.getHeader("Accept")).orElse("");
+    Pattern pattern = Pattern.compile("(.*)-(\\d+\\.\\d+)");
+    Matcher matcher = pattern.matcher(accept);
+    if (isAcceptMatched(matcher)) {
+      return this;
+    } else {
+      Map<String, String[]> params = request.getParameterMap();
+      if (params.entrySet().stream().anyMatch(param -> Objects.equals(param.getKey(), "version"))) {
+        String version = params.get("version")[0];
+        if (isVersionMatched(version)) {
+          return this;
+        }
+      }
+      log.info("No matching version found for request: {}", request);
+      return null;
+    }
+  }
+
+  /*
+   * compareTo 메소드는 두 VersionResourceRequestCondition 객체를 비교한다.
+   */
+  @Override
+  public int compareTo(VersionResourceRequestCondition other, HttpServletRequest request) {
+    return 0;
+  }
+
+  /*
+   * toString 메소드는 VersionResourceRequestCondition 객체의 내용을 문자열로 반환한다.
+   * VersionResourceRequestCondition 객체의 내용은 media, from, to 속성이다.
+   * VersionResourceRequestCondition 객체의 내용을 문자열로 반환하는 과정에서 media 속성은 무시한다.
+   * VersionResourceRequestCondition 객체의 내용을 문자열로 반환하는 과정에서 from, to 속성은 VersionRange 객체의 toString 메소드를 호출한다.
+   * VersionRange 객체의 toString 메소드는 from, to 속성을 문자열로 반환한다.
+   */
+  @Override
+  public String toString() {
+    StringBuilder builder = new StringBuilder();
+    builder.append("version={");
+    builder.append("media=").append(acceptedMediaType).append(",");
+    for (VersionRange range : versions) {
+      builder.append(range).append(",");
+    }
+    builder.append("}");
+
+    return builder.toString();
+
+  }
+
+  @Override
+  protected Collection<?> getContent() {
+    return versions;
+  }
+
+  @Override
+  protected String getToStringInfix() {
+    return " && ";
+  }
+
+  private String combineMediaTypes(VersionResourceRequestCondition other) {
+
     String newMediaType;
 
     if (StringUtils.hasText(this.acceptedMediaType) && StringUtils.hasText(other.acceptedMediaType)
@@ -59,12 +123,15 @@ public class VersionResourceRequestCondition extends
       throw new IllegalArgumentException(
           "Cannot combine media types: " + this.acceptedMediaType + " and "
               + other.acceptedMediaType);
-    } else if (StringUtils.hasText(this.acceptedMediaType)) {
+    }
+
+    if (StringUtils.hasText(this.acceptedMediaType)) {
       newMediaType = this.acceptedMediaType;
+
     } else {
       newMediaType = other.acceptedMediaType;
     }
-    return new VersionResourceRequestCondition(newMediaType, newVersions);
+    return newMediaType;
   }
 
   private boolean isVersionMatched(String version) {
@@ -88,56 +155,5 @@ public class VersionResourceRequestCondition extends
       }
     }
     return false;
-  }
-
-  @Override
-  public VersionResourceRequestCondition getMatchingCondition(HttpServletRequest request) {
-
-    String accept = Optional.ofNullable(request.getHeader("Accept")).orElse("");
-    Pattern pattern = Pattern.compile("(.*)-(\\d+\\.\\d+)");
-    Matcher matcher = pattern.matcher(accept);
-    if (isAcceptMatched(matcher)) {
-      return this;
-    } else {
-      Map<String, String[]> params = request.getParameterMap();
-      if (params.entrySet().stream().anyMatch(param -> Objects.equals(param.getKey(), "version"))) {
-        String version = params.get("version")[0];
-        if (isVersionMatched(version)) {
-          return this;
-        }
-      }
-      log.info("No matching version found for request: {}", request);
-      return null;
-    }
-  }
-
-  @Override
-  protected Collection<?> getContent() {
-    return versions;
-  }
-
-
-  @Override
-  public int compareTo(VersionResourceRequestCondition other, HttpServletRequest request) {
-    return 0;
-  }
-
-  @Override
-  public String toString() {
-    StringBuilder builder = new StringBuilder();
-    builder.append("version={");
-    builder.append("media=").append(acceptedMediaType).append(",");
-    for (VersionRange range : versions) {
-      builder.append(range).append(",");
-    }
-    builder.append("}");
-
-    return builder.toString();
-
-  }
-
-  @Override
-  protected String getToStringInfix() {
-    return " && ";
   }
 }
