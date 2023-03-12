0c21b253ff26dc360a22d465d5ff2ae8bfb2dedc
aeong98 <sy9815@gmail.com>
2023-03-12 14:32:17 +0900

    feat: FirebaseConfig 추가

---
+++ pickly-service/src/main/java/org/pickly/service/common/config/FirebaseConfig.java
@@ -0,0 +1,23 @@
+package org.pickly.service.common.config;
+
+
+import com.google.auth.oauth2.GoogleCredentials;
+import com.google.firebase.FirebaseApp;
+import com.google.firebase.FirebaseOptions;
+import com.google.firebase.auth.FirebaseAuth;
+import java.io.IOException;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class FirebaseConfig {
+  @Bean
+  public FirebaseAuth firebaseAuth() throws IOException{
+    FirebaseOptions options = FirebaseOptions.builder()
+        .setCredentials(GoogleCredentials.getApplicationDefault())
+        .build();
+
+    FirebaseApp.initializeApp(options);
+    return FirebaseAuth.getInstance(FirebaseApp.getInstance());
+  }
+}
