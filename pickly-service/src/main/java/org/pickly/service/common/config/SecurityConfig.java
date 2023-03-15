506b3900dc5e15adf4cff2e99db22ed6058ced55
kimueibin <at365@naver.com>
2023-03-16 00:04:33 +0900

    refactor : Security로 Config 이동

--- pickly-service/src/main/java/org/pickly/service/common/config/SecurityConfig.java
+++ pickly-service/src/main/java/org/pickly/service/common/config/SecurityConfig.java
@@ -1,20 +1,30 @@
 package org.pickly.service.common.config;
 
+import java.util.List;
+import org.pickly.common.access.AccessAutoConfiguration.ApiAccessConfig;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.converter.HttpMessageConverter;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.web.servlet.mvc.WebContentInterceptor;
 
 @Configuration
 @EnableWebSecurity
-public class SecurityConfig {
+public class SecurityConfig extends ApiAccessConfig {
 
   private static final String[] AUTH_WHITELIST = {
       "/api/**", "/graphiql", "/graphql",
       "/swagger-ui/**", "/api-docs", "/swagger-ui-custom.html",
       "/v3/api-docs/**", "/api-docs/**", "/swagger-ui.html"
   };
+
+  public SecurityConfig(
+      List<WebContentInterceptor> interceptors,
+      List<HttpMessageConverter<?>> httpMessageConverters) {
+    super(interceptors, httpMessageConverters);
+  }
 
   @Bean
   public SecurityFilterChain filterChain(HttpSecurity httpSecurity) throws Exception {
