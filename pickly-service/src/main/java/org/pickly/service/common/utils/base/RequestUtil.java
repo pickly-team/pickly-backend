9d9c975ef752982b4890b4240faedf1644c57b05
aeong98 <sy9815@gmail.com>
2023-03-19 20:52:40 +0900

    fix: Bearer 상수화, private 생성자 추가

--- pickly-service/src/main/java/org/pickly/service/common/utils/base/RequestUtil.java
+++ pickly-service/src/main/java/org/pickly/service/common/utils/base/RequestUtil.java
@@ -2,8 +2,13 @@ package org.pickly.service.common.utils.base;
 
 public class RequestUtil {
 
+  private static final String BEARER_PREFIX = "Bearer";
+
+  private RequestUtil() {
+  }
+
   public static String getAuthorizationToken(String header) {
-    if (header == null || !header.startsWith("Bearer ")) {
+    if (header == null || !header.startsWith(BEARER_PREFIX)) {
       throw new IllegalArgumentException("Invalid authorization header");
     }
 
