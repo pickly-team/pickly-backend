7f05e51a4ca1012c4920c424f9a0ec353928cb73
aeong98 <sy9815@gmail.com>
2023-03-19 22:34:38 +0900

    fix: AuthTokenUtil > @Component 추가

--- pickly-service/src/main/java/org/pickly/service/common/utils/base/AuthTokenUtil.java
+++ pickly-service/src/main/java/org/pickly/service/common/utils/base/AuthTokenUtil.java
@@ -5,8 +5,10 @@ import com.google.firebase.auth.FirebaseAuthException;
 import com.google.firebase.auth.FirebaseToken;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
+import org.springframework.stereotype.Component;
 import org.springframework.web.server.ResponseStatusException;
 
+@Component
 @RequiredArgsConstructor
 public class AuthTokenUtil {
 
