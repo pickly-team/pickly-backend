5430cd146e46d2b72b53a7143c390aa13243eb4a
aeong98 <sy9815@gmail.com>
2023-03-19 15:45:50 +0900

    fix: 사용하지 않는 메소드 제거

--- pickly-service/src/main/java/org/pickly/service/member/repository/interfaces/MemberRepository.java
+++ pickly-service/src/main/java/org/pickly/service/member/repository/interfaces/MemberRepository.java
@@ -1,10 +1,8 @@
 package org.pickly.service.member.repository.interfaces;
 
-import java.util.Optional;
 import org.pickly.service.member.entity.Member;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface MemberRepository extends JpaRepository<Member, Long> {
 
-  Optional<Member> findOneByNickname(String nickname);
 }
