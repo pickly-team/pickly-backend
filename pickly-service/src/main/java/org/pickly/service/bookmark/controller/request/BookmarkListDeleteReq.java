672a2c03058a80bcbc03367978f5dc1f4bf6c414
ww8007 <wshmin1234@gmail.com>
2023-03-20 00:20:09 +0900

    feat: 북마크 리스트 삭제 컨트롤러 DTO 추가

--- pickly-service/src/main/java/org/pickly/service/bookmark/controller/request/BookmarkListDeleteReq.java
+++ pickly-service/src/main/java/org/pickly/service/bookmark/controller/request/BookmarkListDeleteReq.java
@@ -1,6 +1,8 @@
 package org.pickly.service.bookmark.controller.request;
 
 import io.swagger.v3.oas.annotations.media.Schema;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Size;
 import java.util.List;
 import lombok.AccessLevel;
 import lombok.AllArgsConstructor;
@@ -11,6 +13,8 @@ import lombok.NoArgsConstructor;
 @AllArgsConstructor
 @Schema(description = "Bookmark list delete request")
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
+@NotNull(message = "북마크 ID 리스트는 필수입니다.")
+@Size(min = 1, message = "북마크 ID 리스트는 최소 1개 이상이어야 합니다.")
 public class BookmarkListDeleteReq {
 
   private List<Long> bookmarkIds;
