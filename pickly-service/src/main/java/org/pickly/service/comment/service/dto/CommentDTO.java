2dbcd4a473378596f2f7814a2d251c99de211a62
EunjiShin <eunji980310@gmail.com>
2023-03-19 19:52:28 +0900

    refactor : LocalDateTime을 FE에서 처리가능하도록 unix timestamp로 변환

--- pickly-service/src/main/java/org/pickly/service/comment/service/dto/CommentDTO.java
+++ pickly-service/src/main/java/org/pickly/service/comment/service/dto/CommentDTO.java
@@ -1,6 +1,7 @@
 package org.pickly.service.comment.service.dto;
 
-import java.time.LocalDateTime;
+import java.time.ZoneOffset;
+import java.time.ZonedDateTime;
 import lombok.Getter;
 import org.pickly.service.comment.entity.Comment;
 
@@ -11,14 +12,17 @@ public class CommentDTO {
   private String member;
   private Boolean isOwnerComment;
   private String content;
-  private LocalDateTime createdDateTime;
+  private Long createdTimestamp;
 
   public CommentDTO(Comment comment) {
+    ZonedDateTime utcDateTime = comment.getCreatedAt().atZone(ZoneOffset.UTC);
+    long unixTimestamp = utcDateTime.toEpochSecond();
+
     this.id = comment.getId();
     this.member = comment.getMember().getNickname();
     this.isOwnerComment = comment.getIsOwnerComment();
     this.content = comment.getContent();
-    this.createdDateTime = comment.getCreatedAt();
+    this.createdTimestamp = unixTimestamp;
   }
 
 }
