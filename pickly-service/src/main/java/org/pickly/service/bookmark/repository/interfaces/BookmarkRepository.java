285a82e08c1922696a53939e61b02acfccf8ddb9
ww8007 <wshmin1234@gmail.com>
2023-03-20 00:17:57 +0900

    feat: deleteBookmarkById, deleteBookmarksByIds 쿼리 추가

--- pickly-service/src/main/java/org/pickly/service/bookmark/repository/interfaces/BookmarkRepository.java
+++ pickly-service/src/main/java/org/pickly/service/bookmark/repository/interfaces/BookmarkRepository.java
@@ -1,10 +1,12 @@
 package org.pickly.service.bookmark.repository.interfaces;
 
+import java.util.List;
 import java.util.Optional;
 import org.pickly.service.bookmark.entity.Bookmark;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
+import org.springframework.data.jpa.repository.Modifying;
 
 public interface BookmarkRepository extends JpaRepository<Bookmark, Long> {
 
@@ -16,4 +18,11 @@ public interface BookmarkRepository extends JpaRepository<Bookmark, Long> {
 
   Long countByMemberId(Long memberId);
 
+  @Modifying
+  @Query("DELETE FROM Bookmark b WHERE b.id = :bookmarkId")
+  Integer deleteBookmarkById(@Param("bookmarkId") Long bookmarkId);
+
+  @Modifying(clearAutomatically = true)
+  @Query("DELETE FROM Bookmark b WHERE b.id IN :bookmarkIds")
+  Integer deleteBookmarksByIds(@Param("bookmarkIds") List<Long> bookmarkIds);
 }
