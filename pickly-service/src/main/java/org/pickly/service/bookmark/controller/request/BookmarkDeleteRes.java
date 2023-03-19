51106c9b4aea93765cfccf4650930bc1e8cace37
ww8007 <wshmin1234@gmail.com>
2023-03-20 00:19:57 +0900

    feat: 북마크 단일 삭제 컨트롤러 DTO 추가

---
+++ pickly-service/src/main/java/org/pickly/service/bookmark/controller/request/BookmarkDeleteRes.java
@@ -0,0 +1,13 @@
+package org.pickly.service.bookmark.controller.request;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Getter
+@AllArgsConstructor
+@NoArgsConstructor
+public class BookmarkDeleteRes {
+
+  private Boolean isDeleted;
+}
