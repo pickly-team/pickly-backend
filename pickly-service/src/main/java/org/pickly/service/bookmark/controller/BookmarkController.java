17f289723e3772797df3dc2fa83acf04b57f76f0
ww8007 <wshmin1234@gmail.com>
2023-03-20 00:16:43 +0900

    refactor: 북마크 삭제, 리스트 삭제 put 변경 및 request body로 처리

--- pickly-service/src/main/java/org/pickly/service/bookmark/controller/BookmarkController.java
+++ pickly-service/src/main/java/org/pickly/service/bookmark/controller/BookmarkController.java
@@ -6,7 +6,6 @@ import io.swagger.v3.oas.annotations.media.Content;
 import io.swagger.v3.oas.annotations.media.Schema;
 import io.swagger.v3.oas.annotations.responses.ApiResponse;
 import io.swagger.v3.oas.annotations.responses.ApiResponses;
-import jakarta.validation.Valid;
 import jakarta.validation.constraints.Positive;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
