4c9caeea9974461fcc564f993b6308c48e52cb85
aeong98 <sy9815@gmail.com>
2023-03-19 20:19:19 +0900

    fix: 새로 래핑한 authtoken util 클래스 사용

--- pickly-service/src/main/java/org/pickly/service/common/filter/JwtFilter.java
+++ pickly-service/src/main/java/org/pickly/service/common/filter/JwtFilter.java
@@ -1,8 +1,6 @@
 package org.pickly.service.common.filter;
 
 
-import com.google.firebase.auth.FirebaseAuth;
-import com.google.firebase.auth.FirebaseAuthException;
 import com.google.firebase.auth.FirebaseToken;
 import jakarta.servlet.FilterChain;
 import jakarta.servlet.ServletException;
@@ -11,7 +9,7 @@ import jakarta.servlet.http.HttpServletResponse;
 import java.io.IOException;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
-import org.apache.http.HttpStatus;
+import org.pickly.service.common.utils.base.AuthToken;
 import org.pickly.service.common.utils.base.RequestUtil;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.stereotype.Component;
@@ -24,24 +22,16 @@ import org.springframework.web.filter.OncePerRequestFilter;
 public class JwtFilter extends OncePerRequestFilter {
 
   private final UserDetailsService userDetailsService;
-  private final FirebaseAuth firebaseAuth;
-  @Override
-  protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain)
-      throws IOException, ServletException {
-    FirebaseToken decodedToken;
+  private final AuthToken authToken;
 
-    try{
-      String bearerToken = RequestUtil.getAuthorizationToken(request.getHeader("Authorization"));
-      decodedToken = firebaseAuth.verifyIdToken(bearerToken);
-    }catch (FirebaseAuthException | IllegalArgumentException e){
-      response.setStatus(HttpStatus.SC_UNAUTHORIZED);
-      response.setContentType("application/json");
-      response.getWriter().write("{\"code\":\"INVALID_TOKEN\", \"message\":\"" + e.getMessage() + "\"}");
-      return;
-    }
+  @Override
+  protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response,
+      FilterChain filterChain)
+      throws IOException, ServletException {
+    String bearerToken = RequestUtil.getAuthorizationToken(request.getHeader("Authorization"));
+    FirebaseToken decodedToken = authToken.getDecodedToken(bearerToken);
 
     //TODO: decodedToken security context에 저장 필요
-
     filterChain.doFilter(request, response);
   }
 }
