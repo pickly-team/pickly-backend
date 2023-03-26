02fac350972b6b67dea77290188d555d223b11b1
ww8007 <wshmin1234@gmail.com>
2023-03-26 20:35:58 +0900

    fix: 중복 선언 삭제

--- pickly-service/src/main/java/org/pickly/service/bookmark/repository/interfaces/BookmarkRepository.java
+++ pickly-service/src/main/java/org/pickly/service/bookmark/repository/interfaces/BookmarkRepository.java
@@ -7,7 +7,6 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Modifying;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
-import org.springframework.data.jpa.repository.Modifying;
 
 public interface BookmarkRepository extends JpaRepository<Bookmark, Long> {
 
