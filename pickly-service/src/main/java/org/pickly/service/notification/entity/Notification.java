6cf9c55a5a2b7f4336d02852f076e90f7969e41d
EunjiShin <eunji980310@gmail.com>
2023-04-05 23:50:34 +0900

    feat : 알림 내역 관리용 Entity 생성

---
+++ pickly-service/src/main/java/org/pickly/service/notification/entity/Notification.java
@@ -0,0 +1,44 @@
+package org.pickly.service.notification.entity;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Convert;
+import jakarta.persistence.Entity;
+import jakarta.persistence.Table;
+import lombok.AccessLevel;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import org.pickly.service.common.utils.base.BaseEntity;
+import org.pickly.service.notification.enums.NotificationType;
+import org.pickly.service.notification.enums.NotificationTypeConverter;
+
+@Entity
+@Getter
+@Builder
+@AllArgsConstructor
+@Table(name = "notification")
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+public class Notification extends BaseEntity {
+
+  @Column(name = "member_id", nullable = false)
+  private Long memberId;
+
+  @Column(nullable = false)
+  private String title;
+
+  @Column(nullable = false)
+  private String message;
+
+  @Column(nullable = false)
+  private boolean isChecked;
+
+  @Column(name = "notification_type", nullable = false)
+  @Convert(converter = NotificationTypeConverter.class)
+  private NotificationType notificationType;
+
+  public void check() {
+    this.isChecked = true;
+  }
+
+}
