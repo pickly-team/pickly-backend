d625d472eb5bc977d7d872e5f37f1caadb87edee
kimueibin <at365@naver.com>
2023-04-09 20:02:38 +0900

    feat : 차단 관련 service 추가

---
+++ pickly-service/src/main/java/org/pickly/service/block/service/dto/BlockBookmarkDTO.java
@@ -0,0 +1,16 @@
+package org.pickly.service.block.service.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Getter
+@NoArgsConstructor
+@AllArgsConstructor
+public class BlockBookmarkDTO {
+
+  private Long id;
+  private String title;
+  private String previewImageUrl;
+
+}
