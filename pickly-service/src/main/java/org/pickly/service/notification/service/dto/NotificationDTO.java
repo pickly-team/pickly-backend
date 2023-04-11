2be8ca137abb96f2955075379b12e1d1a62e9e8b
EunjiShin <eunji980310@gmail.com>
2023-04-12 00:56:04 +0900

    feat : add Notification res DTO

--- pickly-service/src/main/java/org/pickly/service/notification/service/dto/NotificationDTO.java
+++ pickly-service/src/main/java/org/pickly/service/notification/service/dto/NotificationDTO.java
@@ -1,2 +1,14 @@
-package org.pickly.service.notification.service.dto;public class NotificationDTO {
+package org.pickly.service.notification.service.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+
+@Getter
+@AllArgsConstructor
+public class NotificationDTO {
+
+  private Long id;
+  private String title;
+  private String content;
+
 }
