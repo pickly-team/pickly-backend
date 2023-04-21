85d30734adbd52cffa15917a34d49e036b0416ab
kimueibin <at365@naver.com>
2023-04-22 00:26:21 +0900

    refactor : block 관련 repository 내 page 처리 수정

--- pickly-service/src/main/java/org/pickly/service/block/repository/impl/BlockQueryRepositoryImpl.java
+++ pickly-service/src/main/java/org/pickly/service/block/repository/impl/BlockQueryRepositoryImpl.java
@@ -5,13 +5,13 @@ import static org.pickly.service.bookmark.entity.QBookmark.bookmark;
 import static org.pickly.service.member.entity.QMember.member;
 
 import com.querydsl.core.types.Projections;
+import com.querydsl.core.types.dsl.BooleanExpression;
 import com.querydsl.jpa.impl.JPAQueryFactory;
 import java.util.List;
 import lombok.RequiredArgsConstructor;
 import org.pickly.service.block.repository.interfaces.BlockQueryRepository;
 import org.pickly.service.block.service.dto.BlockBookmarkDTO;
 import org.pickly.service.block.service.dto.BlockMemberDTO;
-import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Repository;
 import org.springframework.transaction.annotation.Transactional;
 
@@ -21,41 +21,57 @@ public class BlockQueryRepositoryImpl implements BlockQueryRepository {
 
   private final JPAQueryFactory queryFactory;
 
+  private static final long CHECK_LAST = 1;
 
   @Override
   @Transactional(readOnly = true)
-  public List<BlockMemberDTO> getBlockedMembers(Long blockerId, Pageable page) {
+  public List<BlockMemberDTO> getBlockedMembers(Long blockerId, Long cursorId, Integer size) {
     return queryFactory.select(Projections.fields(BlockMemberDTO.class,
             block.blockee.id,
             block.blockee.nickname,
             block.blockee.profileEmoji
         ))
         .from(block)
-        .leftJoin(member).on(member.id.eq(block.blockee.id)).fetchJoin()
-        .where(block.blocker.id.eq(blockerId)
-            .and(block.bookmark.isNull()))
+        .leftJoin(member).on(member.id.eq(block.blockee.id))
+        .where(
+            block.blocker.id.eq(blockerId),
+            ltblockee(cursorId)
+        .and(block.bookmark.isNull()))
         .orderBy(block.createdAt.desc())
-        .offset(page.getOffset())
-        .limit(page.getPageSize())
+        .limit(size + CHECK_LAST)
         .fetch();
   }
 
   @Override
   @Transactional(readOnly = true)
-  public List<BlockBookmarkDTO> getBlockedBookmarks(Long blockerId, Pageable page) {
+  public List<BlockBookmarkDTO> getBlockedBookmarks(Long blockerId, Long cursorId, Integer size) {
     return queryFactory.select(Projections.fields(BlockBookmarkDTO.class,
             block.bookmark.id,
             block.bookmark.title,
             block.bookmark.previewImageUrl
         ))
         .from(block)
-        .leftJoin(bookmark).on(bookmark.id.eq(block.bookmark.id)).fetchJoin()
-        .where(block.blocker.id.eq(blockerId)
-            .and(block.blockee.isNull()))
+        .leftJoin(bookmark).on(bookmark.id.eq(block.bookmark.id))
+        .where(
+            block.blocker.id.eq(blockerId),
+            ltbookmark(cursorId)
+        .and(block.blockee.isNull()))
         .orderBy(block.createdAt.desc())
-        .offset(page.getOffset())
-        .limit(page.getPageSize())
+        .limit(size + CHECK_LAST)
         .fetch();
   }
 
+  private BooleanExpression ltblockee(final Long cursorId) {
+    if (cursorId == null) {
+      return null;
+    }
+    return block.blockee.id.lt(cursorId);
+  }
+
+  private BooleanExpression ltbookmark(final Long cursorId) {
+    if (cursorId == null) {
+      return null;
+    }
+    return block.bookmark.id.lt(cursorId);
+  }
 }
