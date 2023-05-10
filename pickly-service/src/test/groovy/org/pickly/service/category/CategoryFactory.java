2cbb681d517edda4d2aa79a59e0e59754c7e3110
Judy <judyb0626@gmail.com>
2023-05-10 15:59:52 +0900

    refactor: Delete test code

--- pickly-service/src/test/groovy/org/pickly/service/category/CategoryFactory.java
+++ pickly-service/src/test/groovy/org/pickly/service/category/CategoryFactory.java
@@ -9,26 +9,26 @@ import org.pickly.service.member.entity.Member;
 
 public class CategoryFactory {
 
-  private final MemberFactory memberFactory = new MemberFactory();
-
-
-  public Category testCategory(Member member) {
-    return Category.builder()
-        .member(member)
-        .isAutoDeleteMode(false)
-        .name("당장 내일 안으로 읽어야 하는 자바 면접 질문")
-        .build();
-  }
-
-
-  public List<Category> testCategories(int amount) {
-    return IntStream.rangeClosed(1, amount)
-        .mapToObj(i -> Category.builder()
-            .member(memberFactory.testMember())
-            .isAutoDeleteMode(false)
-            .name("당장 내일 안으로 읽어야 하는 자바 면접 질문" + i)
-            .build())
-        .collect(Collectors.toList());
-  }
+//  private final MemberFactory memberFactory = new MemberFactory();
+//
+//
+//  public Category testCategory(Member member) {
+//    return Category.builder()
+//        .member(member)
+//        .isAutoDeleteMode(false)
+//        .name("당장 내일 안으로 읽어야 하는 자바 면접 질문")
+//        .build();
+//  }
+//
+//
+//  public List<Category> testCategories(int amount) {
+//    return IntStream.rangeClosed(1, amount)
+//        .mapToObj(i -> Category.builder()
+//            .member(memberFactory.testMember())
+//            .isAutoDeleteMode(false)
+//            .name("당장 내일 안으로 읽어야 하는 자바 면접 질문" + i)
+//            .build())
+//        .collect(Collectors.toList());
+//  }
 
 }
