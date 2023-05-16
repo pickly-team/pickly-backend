7081446adf240db216926c12f2cdfa406b9b6c19
EunjiShin <eunji980310@gmail.com>
2023-05-16 22:37:11 +0900

    fix : 카테고리 조회 관련 쿼리에 is_deleted is null 조건 추가

--- pickly-service/src/main/java/org/pickly/service/category/repository/interfaces/CategoryRepository.java
+++ pickly-service/src/main/java/org/pickly/service/category/repository/interfaces/CategoryRepository.java
@@ -14,14 +14,14 @@ public interface CategoryRepository extends JpaRepository<Category, Long> {
   @EntityGraph(attributePaths = {"member"})
   Optional<Category> findById(Long id);
 
-  @Query("select c from Category c where c.member.id = :memberId order by c.createdAt desc limit 1")
+  @Query("select c from Category c where c.member.id = :memberId and c.deletedAt is null order by c.orderNum desc limit 1")
   Category findLastCategoryByMemberId(@Param("memberId") Long memberId);
 
-  @Query("select c from Category c where c.member.id = :memberId order by c.createdAt desc")
+  @Query("select c from Category c where c.member.id = :memberId and c.deletedAt is null order by c.orderNum desc")
   List<Category> findAllCategoryByMemberId(@Param("memberId") Long memberId);
 
   @Modifying
-  @Query("SELECT c FROM Category c WHERE c.id IN :ids")
+  @Query("SELECT c FROM Category c WHERE c.id IN :ids and c.deletedAt is null")
   List<Category> findAllByCategoryId(@Param("ids") List<Long> ids);
 
   @Query(value = "SELECT COUNT(c.id) FROM Category c "
