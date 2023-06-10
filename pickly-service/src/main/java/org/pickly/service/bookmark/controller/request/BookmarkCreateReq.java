ef4fc7cb180b07b1c2f6438a86c067cf8c39726c
EunjiShin <eunji980310@gmail.com>
2023-06-11 00:08:04 +0900

    refactor : input url이 유효하지 않을 때 InvalidValueException 리턴

--- pickly-service/src/main/java/org/pickly/service/bookmark/controller/request/BookmarkCreateReq.java
+++ pickly-service/src/main/java/org/pickly/service/bookmark/controller/request/BookmarkCreateReq.java
@@ -7,7 +7,6 @@ import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
-import org.hibernate.validator.constraints.Length;
 import org.pickly.service.bookmark.entity.Visibility;
 import org.pickly.service.common.utils.validator.UrlCheck;
 
@@ -27,13 +26,6 @@ public class BookmarkCreateReq {
   @NotBlank(message = "URL을 입력해주세요")
   private String url;
 
-  @Length(min = 1, max = 100, message = "최대 100자까지 입력할 수 있습니다.")
-  @NotBlank(message = "제목을 입력해주세요")
-  private String title;
-
-  @Length(min = 1, max = 2000, message = "최대 2000자까지 입력할 수 있습니다.")
-  @NotBlank(message = "프리뷰 이미지 URL을 입력해주세요")
-  private String previewImageUrl;
 
   @NotNull(message = "공개 범위 표시 정보는 필수입니다. ")
   private Visibility visibility;
