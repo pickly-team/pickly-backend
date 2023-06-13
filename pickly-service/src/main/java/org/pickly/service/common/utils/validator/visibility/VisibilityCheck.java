ed882a471f452f7e480d399642a3fc003ba3d7b3
EunjiShin <eunji980310@gmail.com>
2023-06-13 19:59:49 +0900

    fix : visibilityCheck가 사용하는 validator를 수정

--- pickly-service/src/main/java/org/pickly/service/common/utils/validator/visibility/VisibilityCheck.java
+++ pickly-service/src/main/java/org/pickly/service/common/utils/validator/visibility/VisibilityCheck.java
@@ -1,7 +1,6 @@
 package org.pickly.service.common.utils.validator.visibility;
 
 import jakarta.validation.Constraint;
-import org.pickly.service.common.utils.validator.url.UrlValidator;
 
 import java.lang.annotation.ElementType;
 import java.lang.annotation.Retention;
@@ -10,7 +9,7 @@ import java.lang.annotation.Target;
 
 @Target(ElementType.FIELD)
 @Retention(RetentionPolicy.RUNTIME)
-@Constraint(validatedBy = UrlValidator.class)
+@Constraint(validatedBy = VisibilityValidator.class)
 public @interface VisibilityCheck {
 
   String message() default "유효하지 않은 공개 범위 입니다.";
