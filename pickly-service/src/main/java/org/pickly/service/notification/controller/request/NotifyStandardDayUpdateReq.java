59b1e65d6564479e492f47d41e7681cf35ec8a8e
ww8007 <wshmin1234@gmail.com>
2023-07-12 03:56:20 +0900

    fix: 기본 생성자를 생성 하도록 NoArgsConstructor 어노테이션 추가

--- pickly-service/src/main/java/org/pickly/service/notification/controller/request/NotifyStandardDayUpdateReq.java
+++ pickly-service/src/main/java/org/pickly/service/notification/controller/request/NotifyStandardDayUpdateReq.java
@@ -4,9 +4,11 @@ import io.swagger.v3.oas.annotations.media.Schema;
 import jakarta.validation.constraints.NotNull;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 
 @Getter
 @AllArgsConstructor
+@NoArgsConstructor
 @Schema(description = "Notification standard day update request")
 public class NotifyStandardDayUpdateReq {
 
