638b790a15ec0af7b475003e19f871b9db85c941
EunjiShin <eunji980310@gmail.com>
2023-07-24 11:07:33 +0900

    fix : fixed incorrect Visibility Check Validator on bookmark create

--- pickly-service/src/main/java/org/pickly/service/common/utils/validator/url/UrlValidator.java
+++ pickly-service/src/main/java/org/pickly/service/common/utils/validator/url/UrlValidator.java
@@ -2,21 +2,24 @@ package org.pickly.service.common.utils.validator.url;
 
 import jakarta.validation.ConstraintValidator;
 import jakarta.validation.ConstraintValidatorContext;
+import lombok.extern.slf4j.Slf4j;
 import org.pickly.common.error.exception.InvalidValueException;
 
 import java.io.IOException;
 import java.net.HttpURLConnection;
 import java.net.URL;
 
+@Slf4j
 public class UrlValidator implements ConstraintValidator<UrlCheck, String> {
   @Override
   public boolean isValid(String inputUrl, ConstraintValidatorContext context) {
     try {
       URL url = new URL(inputUrl);
       HttpURLConnection connection = (HttpURLConnection) url.openConnection();
-      connection.setRequestMethod("HEAD");
+      connection.setRequestMethod("GET");
       return connection.getResponseCode() == HttpURLConnection.HTTP_OK;
     } catch (IOException e) {
+      log.error("msg = {}", e.getMessage());
       throw new InvalidValueException("유효하지 않은 URL 입니다.");
     }
   }
