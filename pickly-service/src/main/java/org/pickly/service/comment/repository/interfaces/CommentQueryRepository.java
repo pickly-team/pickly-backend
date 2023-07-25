35e7cb91d52e9b97c73fa809d2c3fe7ab069405d
ww8007 <wshmin1234@gmail.com>
2023-07-26 00:35:55 +0900

    fix: commentDTO를 bookmarkCommentDTO로 수정

--- pickly-service/src/main/java/org/pickly/service/comment/repository/interfaces/CommentQueryRepository.java
+++ pickly-service/src/main/java/org/pickly/service/comment/repository/interfaces/CommentQueryRepository.java
@@ -2,11 +2,11 @@ package org.pickly.service.comment.repository.interfaces;
 
 import java.util.List;
 import java.util.Map;
-import org.pickly.service.comment.service.dto.CommentDTO;
+import org.pickly.service.comment.service.dto.BookmarkCommentDTO;
 
 public interface CommentQueryRepository {
 
   Map<Long, Long> findBookmarkCommentCntByMember(Long memberId);
 
-  List<CommentDTO> findComments(Long memberId, Long bookmarkId);
+  List<BookmarkCommentDTO> findComments(Long memberId, Long bookmarkId);
 }
