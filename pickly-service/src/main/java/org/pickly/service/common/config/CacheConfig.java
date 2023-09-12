3d7ea0eb7faa94f508d842d8c16b40aa3c3a9dcd
EunjiShin <eunji980310@gmail.com>
2023-09-12 23:51:27 +0900

    feat: add CacheManager and set TTL

---
+++ pickly-service/src/main/java/org/pickly/service/common/config/CacheConfig.java
@@ -0,0 +1,36 @@
+package org.pickly.service.common.config;
+
+import org.pickly.service.common.utils.cache.ExpireConcurrentMapCache;
+import org.springframework.cache.CacheManager;
+import org.springframework.cache.annotation.EnableCaching;
+import org.springframework.cache.support.SimpleCacheManager;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.scheduling.annotation.Scheduled;
+
+import java.util.Set;
+
+@EnableCaching
+@Configuration
+public class CacheConfig {
+
+  private static final String AUTHENTICATE = "authenticate";
+
+  @Bean
+  public CacheManager cacheManager() {
+    SimpleCacheManager cacheManager = new SimpleCacheManager();
+    cacheManager.setCaches(Set.of(
+        new ExpireConcurrentMapCache(AUTHENTICATE, 5L)
+    ));
+    return cacheManager;
+  }
+
+  @Scheduled(cron = "0 0 0 * * *")
+  private void evict() {
+    ExpireConcurrentMapCache cache = (ExpireConcurrentMapCache) cacheManager().getCache(AUTHENTICATE);
+    if (cache != null) {
+      cache.evictAllExpired();
+    }
+  }
+
+}
