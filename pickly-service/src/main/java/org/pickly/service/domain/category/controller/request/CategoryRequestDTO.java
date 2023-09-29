dc75efe9ee90e4d996a34f8eacc8ae61d9efaa72
EunjiShin <eunji980310@gmail.com>
2023-09-30 02:38:24 +0900

    feat : 카테고리 이름 길이 제한 10글자로 수정

--- pickly-service/src/main/java/org/pickly/service/domain/category/controller/request/CategoryRequestDTO.java
+++ pickly-service/src/main/java/org/pickly/service/domain/category/controller/request/CategoryRequestDTO.java
@@ -5,7 +5,7 @@ import org.hibernate.validator.constraints.Length;
 import org.pickly.service.common.utils.validator.emoji.EmojiCheck;
 
 public record CategoryRequestDTO(
-    @Length(max = 20, message = "카테고리 이름은 20글자가 최대입니다.")
+    @Length(max = 10, message = "카테고리 이름은 20글자가 최대입니다.")
     @NotBlank(message = "카테고리 이름을 입력해주세요.")
     String name,
 
--- pickly-service/src/main/java/org/pickly/service/domain/category/controller/request/CategoryUpdateRequestDTO.java
+++ pickly-service/src/main/java/org/pickly/service/domain/category/controller/request/CategoryUpdateRequestDTO.java
@@ -6,7 +6,7 @@ import org.pickly.service.common.utils.validator.emoji.EmojiCheck;
 
 public record CategoryUpdateRequestDTO(
 
-    @Length(max = 20, message = "카테고리 이름은 20글자가 최대입니다.")
+    @Length(max = 10, message = "카테고리 이름은 20글자가 최대입니다.")
     @NotEmpty(message = "카테고리 이름을 입력해주세요.")
     String name,
 
