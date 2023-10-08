f923729a12cfb63114d4e477ef3c20d9f9216617
EunjiShin <eunji980310@gmail.com>
2023-10-08 23:22:28 +0900

    fix : 친구 follow시 매개변수 순서가 바뀐 오류 수정

--- pickly-service/src/main/java/org/pickly/service/domain/friend/repository/interfaces/FriendRepository.java
+++ pickly-service/src/main/java/org/pickly/service/domain/friend/repository/interfaces/FriendRepository.java
@@ -1,17 +1,18 @@
 package org.pickly.service.domain.friend.repository.interfaces;
 
-import java.util.Optional;
 import org.pickly.service.domain.friend.entity.Friend;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Modifying;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
 
+import java.util.Optional;
+
 public interface FriendRepository extends JpaRepository<Friend, Long> {
 
   @Modifying
-  @Query("delete from Friend f where f.followee.id = :memberId and f.follower.id = :followerId")
-  void unfollow(@Param("followerId") Long followerId, @Param("memberId") Long memberId);
+  @Query("delete from Friend f where f.followee.id = :targetId and f.follower.id = :followerId")
+  void unfollow(@Param("followerId") Long fromMemberId, @Param("targetId") Long toMemberId);
 
   boolean existsByFollowerIdAndFolloweeId(Long followerId, Long followeeId);
 
