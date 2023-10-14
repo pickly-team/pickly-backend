9c8092e2d3f5d0e542cc4b5703bd50f29ece4bcd
EunjiShin <eunji980310@gmail.com>
2023-10-14 17:01:28 +0900

    refactor : Enum을 key로 갖는 hashMap을 enumMap으로 변경

--- pickly-service/src/main/java/org/pickly/service/domain/report/common/ReportServiceFactory.java
+++ pickly-service/src/main/java/org/pickly/service/domain/report/common/ReportServiceFactory.java
@@ -3,14 +3,14 @@ package org.pickly.service.domain.report.common;
 import org.pickly.service.domain.report.service.ReportService;
 import org.springframework.stereotype.Component;
 
-import java.util.HashMap;
+import java.util.EnumMap;
 import java.util.List;
 import java.util.Map;
 
 @Component
 public class ReportServiceFactory {
 
-  private final Map<ReportType, ReportService> reportServiceMap = new HashMap<>();
+  private final Map<ReportType, ReportService> reportServiceMap = new EnumMap<>(ReportType.class);
 
   public ReportServiceFactory(List<ReportService> reportServices) {
     reportServices.forEach(s -> reportServiceMap.put(s.getReportType(), s));
