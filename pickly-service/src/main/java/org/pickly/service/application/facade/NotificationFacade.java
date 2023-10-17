ce6328d4ccf4de0367d63ffc0c5bb315ab4ad223
EunjiShin <eunji980310@gmail.com>
2023-10-18 01:12:33 +0900

    feat : 특정 유저의 모든 알림 삭제 API 추가

--- pickly-service/src/main/java/org/pickly/service/application/facade/NotificationFacade.java
+++ pickly-service/src/main/java/org/pickly/service/application/facade/NotificationFacade.java
@@ -1,6 +1,7 @@
 package org.pickly.service.application.facade;
 
 import lombok.RequiredArgsConstructor;
+import org.pickly.service.domain.member.service.MemberReadService;
 import org.pickly.service.domain.notification.service.NotificationReadService;
 import org.pickly.service.domain.notification.service.NotificationWriteService;
 import org.springframework.stereotype.Service;
@@ -9,6 +10,7 @@ import org.springframework.stereotype.Service;
 @RequiredArgsConstructor
 public class NotificationFacade {
 
+  private final MemberReadService memberReadService;
   private final NotificationReadService notificationReadService;
   private final NotificationWriteService notificationWriteService;
 
@@ -20,6 +22,11 @@ public class NotificationFacade {
   public void delete(Long notificationId) {
     var notification = notificationReadService.findById(notificationId);
     notificationWriteService.delete(notification);
+  }
+
+  public void deleteAllByMember(Long memberId) {
+    var member = memberReadService.findById(memberId);
+    notificationWriteService.deleteByMember(member.getId());
   }
 
 }
