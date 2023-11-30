d67daef468d49db96f62fcdd920ea8a39f7685fb
EunjiShin <eunji980310@gmail.com>
2023-12-01 00:29:42 +0900

    feat : 2개월 이상 로그인하지 않은 유저를 inactive 처리

--- pickly-service/src/main/java/org/pickly/service/domain/member/repository/interfaces/MemberRepository.java
+++ pickly-service/src/main/java/org/pickly/service/domain/member/repository/interfaces/MemberRepository.java
@@ -2,7 +2,12 @@ package org.pickly.service.domain.member.repository.interfaces;
 
 import org.pickly.service.domain.member.entity.Member;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
+import java.time.LocalDateTime;
+import java.util.List;
 import java.util.Optional;
 
 public interface MemberRepository extends JpaRepository<Member, Long> {
@@ -21,4 +26,11 @@ public interface MemberRepository extends JpaRepository<Member, Long> {
   Optional<Member> findByEmailAndDeletedAtIsNull(String email);
 
   Optional<Member> findByEmail(String email);
+
+  List<Member> findByStatusLastLoginAtBefore(LocalDateTime inactiveStandard);
+
+  @Modifying
+  @Query("update Member m set m.status.isInactive = true where m.id in :ids")
+  void convertToSleeper(@Param("ids") List<Long> memberIds);
+
 }
